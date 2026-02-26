6c6ec217230c8a1ca130eecd576ed12b7b82ddf9
Junwon <26790365+junwonp@users.noreply.github.com>
2026-02-26 15:54:46 +0900

    Export `IconSymbolName` type in icon-symbol.tsx for improved accessibility in icon mapping.

--- components/ui/icon-symbol.tsx
+++ components/ui/icon-symbol.tsx
@@ -6,7 +6,7 @@ import { SymbolViewProps, SymbolWeight } from 'expo-symbols';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 type IconMapping = Record<SymbolViewProps['name'], ComponentProps<typeof MaterialIcons>['name']>;
-type IconSymbolName = keyof typeof MAPPING;
+export type IconSymbolName = keyof typeof MAPPING;
 
 /**
  * Add your SF Symbols to Material Icons mappings here.
