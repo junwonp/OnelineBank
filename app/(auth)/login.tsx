703e3972c2b76b390834f519f225e80295f1802e
Junwon <26790365+junwonp@users.noreply.github.com>
2026-02-28 21:49:40 +0900

    Refactor Login component to improve user experience and integrate Snackbar notifications
    - Removed unused imports and streamlined state management for email and password inputs.
    - Replaced error message handling with Snackbar notifications for better user feedback.
    - Enhanced layout with a logo image and improved styling for input fields and buttons.
    - Updated the structure of the component for better readability and maintainability.

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,30 +1,29 @@
-import { useRef, useState } from 'react';
-import { TextInput, View } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useState } from 'react';
+import { View } from 'react-native';
+import { Image } from 'expo-image';
 import { useRouter } from 'expo-router';
 
+import { useSnackbar } from '@/components/providers/snackbar-provider';
 import Button from '@/components/ui/button';
 import Input from '@/components/ui/input';
-import Text from '@/components/ui/text';
 import { signInWithEmail } from '@/features/auth/api/auth';
 import { useAuthStore } from '@/features/auth/store/useAuthStore';
 import { removeWhitespace } from '@/utils/string';
 import { isValidEmail } from '@/utils/validation';
 
 const Login = () => {
+  const { setUser } = useAuthStore();
+  const router = useRouter();
+  const { setSnackbarMessage } = useSnackbar();
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
   const [disabled, setDisabled] = useState(true);
-  const passwordRef = useRef<TextInput>(null);
-  const insets = useSafeAreaInsets();
-  const router = useRouter();
-  const { setUser } = useAuthStore();
 
   const handleEmailChange = (email: string) => {
     const changedEmail = removeWhitespace(email);
     setEmail(changedEmail);
-    setErrorMessage(!isValidEmail(changedEmail) ? '이메일 형식을 확인해주세요.' : '');
+    setSnackbarMessage(!isValidEmail(changedEmail) ? '이메일 형식을 확인해주세요.' : '');
   };
 
   const handlePasswordChange = (password: string) => {
@@ -37,57 +36,56 @@ const Login = () => {
       setUser(result.user || null);
       router.push('/');
     } else {
-      setErrorMessage(result.error || '');
+      setSnackbarMessage(result.error || '');
     }
   };
 
   return (
-    <View className="bg-background flex-1 px-5">
-      <View
-        style={{
-          flex: 1,
-          paddingTop: insets.top,
-          paddingBottom: insets.bottom,
-          alignItems: 'center',
-        }}
-        className="bg-background flex-1 items-center px-5"
-      >
-        {/* <Image source={images.logo} imageStyle={{ borderRadius: 30 }} /> */}
-        <Input
-          label="이메일"
-          value={email}
-          onChangeText={handleEmailChange}
-          onSubmitEditing={handleLoginButtonPress}
-          placeholder="이메일을 입력해주세요."
-          keyType="email-address"
-          returnKeyType="next"
+    <View className="bg-background py-safe flex-1 px-5">
+      <View className="w-full flex-1 items-center justify-center gap-5">
+        <Image
+          source={
+            'https://firebasestorage.googleapis.com/v0/b/react-native-chat-d43a3.appspot.com/o/logo.png?alt=media'
+          }
+          style={{ width: 100, height: 100, borderRadius: 20 }}
         />
-        <Input
-          ref={passwordRef}
-          label="비밀번호"
-          value={password}
-          onChangeText={handlePasswordChange}
-          onSubmitEditing={() => {}}
-          placeholder="비밀번호를 입력해주세요."
-          returnKeyType="done"
-          isPassword
-        />
-        <Text className="text-errorText mb-2.5 w-full text-left leading-5">{errorMessage}</Text>
-        <View className="w-full flex-col justify-between gap-2.5">
-          <Button
-            label="로그인"
-            onPress={handleLoginButtonPress}
-            disabled={!disabled}
-            variant="default"
-            size="default"
+
+        <View className="gap-5">
+          <Input
+            label="이메일"
+            value={email}
+            onChangeText={handleEmailChange}
+            onSubmitEditing={handleLoginButtonPress}
+            placeholder="이메일을 입력해주세요."
+            keyboardType="email-address"
+            returnKeyType="next"
           />
-          <Button
-            label="회원가입"
-            onPress={() => router.push('/sign-up')}
-            variant="outline"
-            size="default"
+          <Input
+            label="비밀번호"
+            value={password}
+            onChangeText={handlePasswordChange}
+            onSubmitEditing={() => {}}
+            placeholder="비밀번호를 입력해주세요."
+            returnKeyType="done"
+            isPassword
           />
         </View>
+      </View>
+      <View className="flex-1" />
+      <View className="w-full gap-2.5">
+        <Button
+          label="로그인"
+          onPress={handleLoginButtonPress}
+          disabled={!disabled}
+          variant="default"
+          size="default"
+        />
+        <Button
+          label="회원가입"
+          onPress={() => router.push('/sign-up')}
+          variant="outline"
+          size="default"
+        />
       </View>
     </View>
   );
